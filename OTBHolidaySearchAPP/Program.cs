fa5dc37521556327730956435f502ca0d1ab99a9
sheryjebu <sheryjebu@gmail.com>
2022-07-21 17:13:22 +0100

    Intial Commit: Added Model Folder to hold model classes. Newtonsoft.Json Pkg. Pusedocode Comments

--- OTBHolidaySearchAPP/Program.cs
+++ OTBHolidaySearchAPP/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information/
 
+/* Class1. to store Fight objects
+ * Class2. to store Hotel objects
+ * Class Search - with search() based on user inputs/ output()
+ * Class ToLoadJson data
+ */
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
